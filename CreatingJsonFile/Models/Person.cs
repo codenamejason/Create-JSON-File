e63a3dba7d19014eff026b52bc55c79bd6fc8f45
ravi <rpatel@webcontentor.com>
2015-10-25 19:48:03 +0530

    first

---
+++ CreatingJsonFile/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CreatingJsonFile.Models
+{       
+    
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string  City { get; set; }
+
+    }
+}
